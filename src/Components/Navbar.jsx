acf4a8c1f9098049bc23c3906cb710f86635faac
Harshit <harshitsingharya24@gmail.com>
2024-04-17 14:54:07 +0530

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -8,15 +8,18 @@ export default function Navbar() {
     function toggle() {
         Mobile ? setMobile(false) : setMobile(true);
     }
+    function click(){
+        setMobile(false);
+    }
     return (
         <div>
             <nav className={`nav-container`}>
                 <img src={logo} className='logo' />
-                <ul className={Mobile ?'' :'mobile-menu'}>
-                    <Link className='li' to='/'>Home</Link>
-                    <Link className='li' to='/about'>About Us</Link>
-                    <Link to={'/campus'} className='li'>Campus</Link>
-                    <Link to={'/contact'} className='li'><button className='btn'>Contact Us</button></Link>
+                <ul className={Mobile ? '' : 'mobile-menu'}>
+                    <Link onClick={click} className='li' to='/'>Home</Link>
+                    <Link onClick={click} className='li' to='/about'>About Us</Link>
+                    <Link onClick={click} to={'/campus'} className='li'>Campus</Link>
+                    <Link onClick={click} to={'/contact'} className='li'><button className='btn'>Contact Us</button></Link>
                 </ul>
                 <img src={menu} alt="" className='menu' onClick={toggle} />
             </nav>
